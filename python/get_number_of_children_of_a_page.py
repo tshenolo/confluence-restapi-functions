b2bc77b6820a6fdcc4f6773eb79c9da2d5198478
codingweekly <codingweekly@gmail.com>
2023-05-26 09:38:25 -0400

    Added comments to python files

---
+++ python/get_number_of_children_of_a_page.py
@@ -0,0 +1,10 @@
+# 
+# This script will get the total number of children for a specific page via the Confluence REST API
+# Usage:
+# Update the following variables before you run this script:
+# - YOUR_SERVER
+# - YOUR_PERSONAL_ACCESS_TOKEN (found @ YOUR_SERVER/plugins/personalaccesstokens/usertokens.action)
+# - YOUR_PAGEID 
+# 
+# Open your CLI execute the following command:
+# py get_number_of_children_of_a_page.py
---
+++ python/get_page_content.py
@@ -0,0 +1,10 @@
+# 
+# This script will get contents of a page via the Confluence REST API
+# Usage:
+# Update the following variables before you run this script:
+# - YOUR_SERVER
+# - YOUR_PERSONAL_ACCESS_TOKEN (found @ YOUR_SERVER/plugins/personalaccesstokens/usertokens.action)
+# - YOUR_PAGEID 
+# 
+# Open your CLI execute the following command:
+# py get_page_content.py
